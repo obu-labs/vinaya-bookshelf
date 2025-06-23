50e73f92de88d1767dc7ba0033590bd23a52ebc6
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2025-06-23 15:01:53 +0700

    some minor improvements to the appsettings checker

--- src/appsettings.ts
+++ src/appsettings.ts
@@ -9,47 +9,82 @@ const SETTINGS_CHECK_INTERVAL = 20 * 1000;
 const ENFORCED_APP_SETTINGS: Record<string, any> = {
   "newLinkFormat": "relative",
   "useMarkdownLinks": true,
+  "newFileLocation": "folder",
+  /* TODO: get these from plugin settings
+  "newFileFolderPath": newFileFolderPath,
+  "attachmentFolderPath": `${newFileFolderPath}/attachments`
+  */
 }
 
 export async function checkAppSettings(plugin: VinayaNotebookPlugin) {
+  if (document.getElementsByClassName("modal mod-settings").length > 0) {
+    // The settings panel is currently open
+    // Check again soon
+    console.log("Skipping check while settings open...")
+    plugin.settingsChecker = setTimeout(async () => {
+      await checkAppSettings(plugin);
+    }, 800);
+    return;
+  }
+  console.log("Checking settings...")
   const config_dir = plugin.app.vault.configDir;
   const app_settings_path = `${config_dir}/app.json`;
   let config_json = await plugin.app.vault.adapter.read(app_settings_path);
   let config: Record<string, any> = {};
-  let changed_settings: Array<string> = [];
+  let changed_settings: Map<string, string> = new Map<string, string>();
   if (config) {
     config = JSON.parse(config_json);
   }
   for (const key in ENFORCED_APP_SETTINGS) {
     if (config[key] !== ENFORCED_APP_SETTINGS[key]) {
+      changed_settings.set(key, config[key].toString());
       config[key] = ENFORCED_APP_SETTINGS[key];
-      changed_settings.push(key);
     }
   }
-  if (changed_settings.length > 0) {
+  if (changed_settings.size > 0) {
     let settings_string = "your setting";
-    if (changed_settings.length > 1) {
+    if (changed_settings.size > 1) {
       settings_string += "s";
     }
     settings_string += ":";
-    for (let i = 0; i < changed_settings.length; i++) {
-      const setting_name = changed_settings[i];
+    let i = 0;
+    for (const [setting_name, bad_value] of changed_settings.entries()) {
       settings_string += ` "${setting_name}"`;
-      if (changed_settings.length > 2) {
+      if (changed_settings.size > 2) {
         settings_string += ",";
       }
-      if (i == changed_settings.length - 2 && changed_settings.length >= 2) {
+      if (i == changed_settings.size - 2 && changed_settings.size >= 2) {
         settings_string += " and";
       }
+      i += 1;
     }
-    if (changed_settings.length > 1) {
-      settings_string += " were changed";
+    if (changed_settings.size > 1) {
+      settings_string += " were";
     } else {
-      settings_string += " was changed";
+      settings_string += " was";
+    }
+    settings_string += " changed to";
+    i = 0;
+    for (const [setting_name, bad_value] of changed_settings.entries()) {
+      settings_string += ` "${bad_value}"`;
+      if (changed_settings.size > 2) {
+        settings_string += ",";
+      }
+      if (i == changed_settings.size - 2 && changed_settings.size >= 2) {
+        settings_string += " and";
+      }
+      i += 1;
+    }
+    if (changed_settings.size > 1) {
+      settings_string += " respectively.";
+    } else {
+      settings_string += " from the default value of \"";
+      settings_string += ENFORCED_APP_SETTINGS[changed_settings.keys().next().value];
+      settings_string += ".\"";
     }
     const disable_checker = await confirmationModal(
       "You changed your vault settings",
-      `It seems that ${settings_string} from the defaults. This may cause the notes that you take to become incompatible with other applications. Would you like to revert to the recommended settings?`,
+      `It seems that ${settings_string} This may cause the notes that you take to become incompatible with other applications. Would you like to revert to the recommended settings?`,
       plugin.app,
       "Stop Checking Compatibility",
       "Revert Settings to Safer Defaults",
